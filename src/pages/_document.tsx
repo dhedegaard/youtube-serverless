08cd153656196f414e856e6a494e58721e798692
Dennis Hedegaard <dennis@dhedegaard.dk>
2022-02-03 17:19:44 +0100

    converted document to FC

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,13 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+const Document = () => (
+  <Html lang="en">
+    <Head />
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
