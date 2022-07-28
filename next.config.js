04643916e67f4fe6301d8f498af0d24aea9fd1bb
Dennis Hedegaard <dennis@dhedegaard.dk>
2022-07-28 13:54:37 +0200

    bumped lots of packages + use emotion plugin

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+/**
+ * @type {import('next').NextConfig}
+ */
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["yt3.ggpht.com"],
+  },
+  compiler: {
+    emotion: true,
   },
 };
