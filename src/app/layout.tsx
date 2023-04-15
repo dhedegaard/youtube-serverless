07279c6e2e2d78d0397acbf0c65a07e5ece59e27
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-04-15 18:32:22 +0200

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
+import { Metadata } from "next";
 import { ReactNode } from "react";
 import "../styles/globals.css";
 
+export const metadata: Metadata = {
+  title: "Latest youtube videos",
+  description: "Latest youtube videos",
+};
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
