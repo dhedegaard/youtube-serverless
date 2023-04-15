c861b7484f2e5696ac2bb1d78af12022dc45f5ec
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-04-15 19:02:29 +0200

    improvements to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,27 @@ import "../styles/globals.css";
 
 export const metadata: Metadata = {
   title: "New youtube videos",
-  description: "New youtube videos",
+  description: "Aggregating youtube videos since 2015",
+  viewport: "minimum-scale=1, initial-scale=1, width=device-width",
+  icons: {
+    icon: "/favicon.png",
+    apple: "/favicon.png",
+  },
+  appleWebApp: true,
+  openGraph: {
+    siteName: "Youtube",
+    url: "https://ut.dhedegaard.dk/",
+    title: "New youtube videos",
+    description: "Aggregating youtube videos since 2015",
+  },
+  themeColor: "#222222",
 };
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <head />
+      <head>
+        <meta charSet="utf-8" />
+      </head>
       <body>{children}</body>
     </html>
   );
