21b7d3b2e3a1a6a0f0f33cfaef81c36a29e701fb
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-05-23 19:57:28 +0200

    schema validate data from youtube api

--- src/data/youtube/index.ts
+++ src/data/youtube/index.ts
@@ -1,22 +1,33 @@
+import Z from "zod";
+
 const apiKey = process.env.YOUTUBE_API_KEY;
 if (apiKey == null) {
   throw new Error("Missing YOUTUBE_API_KEY");
 }
 
-export const getChannelInfo = async (
-  channelId: string
-): Promise<{
-  items: {
-    id: string;
-    snippet: {
-      title: string;
-      description: string;
-      customUrl: string;
-      thumbnails: { high: { url: string } };
-    };
-    contentDetails: { relatedPlaylists: { uploads: string } };
-  }[];
-}> => {
+const channelInfoSchema = Z.object({
+  items: Z.array(
+    Z.object({
+      id: Z.string().nonempty(),
+      snippet: Z.object({
+        title: Z.string().nonempty(),
+        description: Z.string(),
+        customUrl: Z.string(),
+        thumbnails: Z.object({
+          high: Z.object({
+            url: Z.string().nonempty(),
+          }),
+        }),
+      }),
+      contentDetails: Z.object({
+        relatedPlaylists: Z.object({
+          uploads: Z.string().nonempty(),
+        }),
+      }),
+    })
+  ),
+});
+export const getChannelInfo = async (channelId: string) => {
   const params = new URLSearchParams();
   params.set("part", "snippet,contentDetails");
   params.set("id", channelId);
@@ -29,9 +40,56 @@ export const getChannelInfo = async (
       `Unable to get channel info: ${resp.status} ${resp.statusText}`
     );
   }
-  return await resp.json();
+  return await resp
+    .json()
+    .then((data: unknown) => channelInfoSchema.parseAsync(data));
 };
 
+const videoSchema = Z.object({
+  nextPageToken: Z.string().optional(),
+  items: Z.array(
+    Z.object({
+      contentDetails: Z.object({
+        videoId: Z.string().nonempty(),
+        videoPublishedAt: Z.string().nonempty(),
+      }),
+      snippet: Z.object({
+        publishedAt: Z.string().nonempty(),
+        channelId: Z.string().nonempty(),
+        title: Z.string().nonempty(),
+        description: Z.string(),
+        thumbnails: Z.object({
+          default: Z.object({
+            url: Z.string().nonempty(),
+            width: Z.number(),
+            height: Z.number(),
+          }),
+          medium: Z.object({
+            url: Z.string().nonempty(),
+            width: Z.number(),
+            height: Z.number(),
+          }),
+          high: Z.object({
+            url: Z.string().nonempty(),
+            width: Z.number(),
+            height: Z.number(),
+          }),
+          standard: Z.object({
+            url: Z.string().nonempty(),
+            width: Z.number(),
+            height: Z.number(),
+          }).optional(),
+          maxres: Z.object({
+            url: Z.string().nonempty(),
+            width: Z.number(),
+            height: Z.number(),
+          }).optional(),
+        }),
+        channelTitle: Z.string().nonempty(),
+      }),
+    })
+  ),
+});
 export const getVideosForChannelId = async (channelId: string) => {
   const params = new URLSearchParams();
   params.set("part", "contentDetails,snippet");
@@ -42,28 +100,8 @@ export const getVideosForChannelId = async (channelId: string) => {
     `https://www.googleapis.com/youtube/v3/playlistItems?${params.toString()}`,
     {}
   );
-  const data = (await resp.json()) as {
-    nextPageToken: string | undefined;
-    items: Array<{
-      contentDetails: {
-        videoId: string;
-        videoPublishedAt: string;
-      };
-      snippet: {
-        publishedAt: string;
-        channelId: string;
-        title: string;
-        description: string;
-        thumbnails: {
-          default: { url: string; width: number; height: number };
-          medium: { url: string; width: number; height: number };
-          high: { url: string; width: number; height: number };
-          standard: { url: string; width: number; height: number };
-          maxres: { url: string; width: number; height: number };
-        };
-        channelTitle: string;
-      };
-    }>;
-  };
-  return [...data.items];
+  const data = await resp
+    .json()
+    .then((data: unknown) => videoSchema.parseAsync(data));
+  return data.items;
 };
