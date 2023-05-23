b86593dcc757dc7c578453394aac73f826fd39df
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-05-23 17:13:38 +0200

    fixed add-channel route

--- src/app/api/add-channel/route.ts
+++ src/app/api/add-channel/route.ts
@@ -19,27 +19,34 @@ export const POST = async (request: NextRequest) => {
     );
   }
 
-  const {
-    items: [item],
-  } = await getChannelInfo(channelId);
-  if (item == null) {
+  try {
+    const {
+      items: [item],
+    } = await getChannelInfo(channelId);
+    if (item == null) {
+      return NextResponse.json(
+        { error: `Channel with id not found: ${channelId}` },
+        { status: 404 }
+      );
+    }
+
+    const channel = await updateChannel({
+      PK: { S: "CHANNELS" },
+      SK: { S: `CHANNEL#${item.id}` },
+      channelId: { S: item.id },
+      channelTitle: { S: item.snippet.title },
+      playlist: { S: item.contentDetails.relatedPlaylists.uploads },
+      thumbnail: { S: item.snippet.thumbnails.high.url },
+      channelLink: { S: `https://www.youtube.com/channel/${item.id}` },
+      channelThumbnail: { S: item.snippet.thumbnails.high.url },
+    });
+
+    return NextResponse.json(channel);
+  } catch (error: unknown) {
+    console.error(error);
     return NextResponse.json(
-      { error: `Channel with id not found: ${channelId}` },
-      { status: 404 }
+      { error: "Some unknown error happened, check the logs" },
+      { status: 500 }
     );
   }
-
-  const channel = await updateChannel({
-    PK: { S: "CHANNELS" },
-    SK: { S: `CHANNEL#${item.id}` },
-    channelId: { S: item.id },
-    channelTitle: { S: item.snippet.title },
-    videoIds: undefined,
-    playlist: { S: item.contentDetails.relatedPlaylists.uploads },
-    thumbnail: { S: item.snippet.thumbnails.high.url },
-    channelLink: { S: `https://www.youtube.com/channel/${item.id}` },
-    channelThumbnail: { S: item.snippet.thumbnails.high.url },
-  });
-
-  return NextResponse.json(channel);
 };
