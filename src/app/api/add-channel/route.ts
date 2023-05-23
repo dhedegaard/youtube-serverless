63adcd326263a1670d9da872ad86e7b99574ca1f
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-05-23 16:59:05 +0200

    schemas for video and channel data in dynamodb + API route mapping that doesn't currently work.

---
+++ src/app/api/add-channel/route.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from "next/server";
+import { updateChannel } from "../../../data";
+import { getChannelInfo } from "../../../data/youtube";
+
+export const POST = async (request: NextRequest) => {
+  if (request.headers.get("authorization") !== process.env.SECRET) {
+    return NextResponse.json(
+      { error: "Missing or bad authorization header" },
+      { status: 401 }
+    );
+  }
+
+  const url = new URL(request.url);
+  const channelId = url.searchParams.get("channelId");
+  if (typeof channelId !== "string" || channelId.length < 1) {
+    return NextResponse.json(
+      { error: "Missing channelId query param" },
+      { status: 400 }
+    );
+  }
+
+  const {
+    items: [item],
+  } = await getChannelInfo(channelId);
+  if (item == null) {
+    return NextResponse.json(
+      { error: `Channel with id not found: ${channelId}` },
+      { status: 404 }
+    );
+  }
+
+  const channel = await updateChannel({
+    PK: { S: "CHANNELS" },
+    SK: { S: `CHANNEL#${item.id}` },
+    channelId: { S: item.id },
+    channelTitle: { S: item.snippet.title },
+    videoIds: undefined,
+    playlist: { S: item.contentDetails.relatedPlaylists.uploads },
+    thumbnail: { S: item.snippet.thumbnails.high.url },
+    channelLink: { S: `https://www.youtube.com/channel/${item.id}` },
+    channelThumbnail: { S: item.snippet.thumbnails.high.url },
+  });
+
+  return NextResponse.json(channel);
+};
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,5 @@
 import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import Z, { TypeOf, object } from "zod";
 
 const TableName = process.env.AWS_DYNAMODB_TABLE;
 if (TableName == null) {
@@ -22,73 +23,81 @@ const db = new DynamoDB({
   region: process.env.AWS_DYNAMODB_REGION,
 });
 
-interface Channel {
-  PK: { S: string };
-  SK: { S: string };
-  channelId: { S: string };
-  channelTitle: { S: string };
-  playlist: { S: string };
-  videoIds: { SS: string[] } | undefined;
-  thumbnail: { S: string };
-  channelThumbnail: { S: string };
-  channelLink: { S: string };
-}
+const channelSchema = Z.object({
+  PK: Z.object({ S: Z.string() }),
+  SK: Z.object({ S: Z.string() }),
+  channelId: Z.object({ S: Z.string() }),
+  channelTitle: Z.object({ S: Z.string() }),
+  playlist: Z.object({ S: Z.string() }),
+  videoIds: Z.object({ SS: Z.array(Z.string()) }).optional(),
+  thumbnail: Z.object({ S: Z.string() }),
+  channelThumbnail: Z.object({ S: Z.string() }),
+  channelLink: Z.object({ S: Z.string() }),
+});
+interface Channel extends TypeOf<typeof channelSchema> {}
 
-export const getChannels = async (): Promise<Array<Channel>> => {
+const videoSchema = Z.object({
+  PK: Z.object({ S: Z.literal("VIDEOS") }),
+  SK: Z.object({ S: Z.string().startsWith("VIDEO#") }),
+  channelId: Z.object({ S: Z.string() }),
+  videoId: Z.object({ S: Z.string() }),
+  videoPublishedAt: Z.object({ S: Z.string() }),
+  thumbnail: Z.object({ S: Z.string() }),
+  channelTitle: Z.object({ S: Z.string() }),
+  channelThumbnail: Z.object({ S: Z.string() }),
+  channelLink: Z.object({ S: Z.string() }),
+  title: Z.object({ S: Z.string() }),
+});
+interface Video extends TypeOf<typeof videoSchema> {}
+
+export const getChannels = async () => {
   const resp = await db.query({
     TableName,
     KeyConditionExpression: "PK = :pk",
     ExpressionAttributeValues: { ":pk": { S: "CHANNELS" } },
   });
-  return resp.Items as any;
+  return await Z.array(channelSchema).parseAsync(resp.Items ?? []);
 };
 
 export const updateChannel = async (channel: Channel) => {
-  await db.putItem({ Item: channel as any, TableName });
+  const parsedChannel = await channelSchema.parseAsync(channel);
+  await db.putItem({
+    Item: parsedChannel,
+    TableName,
+  });
   return channel;
 };
 
 export const putVideos = async (video: {
-  videoId: string;
-  videoPublishedAt: string;
-  channelId: string;
-  channelTitle: string;
-  thumbnail: string;
-  title: string;
-  channelThumbnail: string;
-  channelLink: string;
+  readonly videoId: string;
+  readonly videoPublishedAt: string;
+  readonly channelId: string;
+  readonly channelTitle: string;
+  readonly thumbnail: string;
+  readonly title: string;
+  readonly channelThumbnail: string;
+  readonly channelLink: string;
 }) => {
+  const validatedItem = await videoSchema.parseAsync({
+    PK: { S: "VIDEOS" },
+    SK: { S: `VIDEO#${video.videoId}` },
+    channelId: { S: video.channelId },
+    videoId: { S: video.videoId },
+    videoPublishedAt: { S: video.videoPublishedAt },
+    thumbnail: { S: video.thumbnail },
+    channelTitle: { S: video.channelTitle },
+    channelThumbnail: { S: video.channelThumbnail },
+    channelLink: { S: video.channelLink },
+    title: { S: video.title },
+  });
   await db.putItem({
-    Item: {
-      PK: { S: "VIDEOS" },
-      SK: { S: `VIDEO#${video.videoId}` },
-      channelId: { S: video.channelId },
-      videoId: { S: video.videoId },
-      videoPublishedAt: { S: video.videoPublishedAt },
-      thumbnail: { S: video.thumbnail },
-      channelTitle: { S: video.channelTitle },
-      channelThumbnail: { S: video.channelThumbnail },
-      channelLink: { S: video.channelLink },
-      title: { S: video.title },
-    },
+    Item: validatedItem,
     TableName,
   });
+  return validatedItem;
 };
 
-export const getLatestVideos = async (): Promise<
-  Array<{
-    PK: { S: string };
-    SK: { S: string };
-    channelId: { S: string };
-    videoId: { S: string };
-    videoPublishedAt: { S: string };
-    thumbnail: { S: string };
-    channelTitle: { S: string };
-    channelLink: { S: string };
-    channelThumbnail: { S: string };
-    title: { S: string };
-  }>
-> => {
+export const getLatestVideos = async () => {
   const resp = await db.query({
     TableName,
     IndexName: "PK_videoPublishedAt",
@@ -97,5 +106,5 @@ export const getLatestVideos = async (): Promise<
     KeyConditionExpression: "PK = :pk",
     ExpressionAttributeValues: { ":pk": { S: "VIDEOS" } },
   });
-  return (resp.Items as any) ?? [];
+  return await Z.array(videoSchema).parseAsync(resp.Items ?? []);
 };
