f557bd96e8ff7038dbd384c949822a83755188ca
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-08-21 21:53:07 +0200

    moved another env var to schema validation

--- src/clients/dynamodb/index.ts
+++ src/clients/dynamodb/index.ts
@@ -6,20 +6,17 @@ import {
 } from "@aws-sdk/client-dynamodb";
 import { z } from "zod";
 
-const TableName = process.env.AWS_DYNAMODB_TABLE;
-if (TableName == null) {
-  throw new Error("Missing AWS_DYNAMODB_TABLE");
-}
-
 const envSchema = z.object({
   AWS_DYNAMODB_ACCESS_KEY: z.string().nonempty(),
   AWS_DYNAMODB_SECRET_ACCESS_KEY: z.string().nonempty(),
   AWS_DYNAMODB_REGION: z.string().nonempty(),
+  AWS_DYNAMODB_TABLE: z.string().nonempty(),
 });
 const {
   AWS_DYNAMODB_ACCESS_KEY,
   AWS_DYNAMODB_REGION,
   AWS_DYNAMODB_SECRET_ACCESS_KEY,
+  AWS_DYNAMODB_TABLE: TableName,
 } = envSchema.parse(
   Object.fromEntries(
     Object.keys(envSchema.shape).map((key) => [key, process.env[key]])
