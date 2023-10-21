0352c433487a9b54618c75b6e1ef24f209674b74
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-10-21 18:51:14 +0200

    added nullable ids to the common models.

--- src/schemas/channel.ts
+++ src/schemas/channel.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod'
 
 export const channelSchema = z.object({
+  /** Determine by the database backend, null if we're creating something new. */
+  id: z.nullable(z.string()),
   channelId: z.string(),
   channelTitle: z.string(),
   playlist: z.string(),
--- src/schemas/video.ts
+++ src/schemas/video.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod'
 
 export const videoSchema = z.object({
+  /** Determine by the database backend, null if we're creating something new. */
+  id: z.nullable(z.string()),
   channelId: z.string(),
   videoId: z.string(),
   videoPublishedAt: z.string(),
