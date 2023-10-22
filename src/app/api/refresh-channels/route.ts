e75552e7f1c83ded1f48e13c0399f53eabd38b12
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-10-22 14:12:17 +0200

    avoid emitting all the videoIds from the refresh-channels endpoint

--- src/app/api/refresh-channels/route.ts
+++ src/app/api/refresh-channels/route.ts
@@ -2,6 +2,7 @@ import { revalidatePath } from 'next/cache'
 import { NextRequest, NextResponse } from 'next/server'
 import { getChannels, updateChannel } from '../../../clients/dynamodb'
 import { getChannelInfo } from '../../../clients/youtube'
+import { Channel } from '../../../models/channel'
 import { isApiRequestAuthenticated } from '../../../utils/api-helpers'
 
 export const POST = async (request: NextRequest) => {
@@ -16,17 +17,25 @@ export const POST = async (request: NextRequest) => {
       if (item == null) {
         continue
       }
-      channel.channelTitle = item.snippet.title
-      channel.channelThumbnail = item.snippet.thumbnails.high.url
-      channel.playlist = item.contentDetails.relatedPlaylists.uploads
-      channel.thumbnail = item.snippet.thumbnails.high.url
-      channel.channelLink = `https://www.youtube.com/channel/${channel.channelId}`
-      await updateChannel({ channel })
+      const toBeUpdated: Channel = {
+        ...channel,
+        channelTitle: item.snippet.title,
+        channelThumbnail: item.snippet.thumbnails.high.url,
+        playlist: item.contentDetails.relatedPlaylists.uploads,
+        thumbnail: item.snippet.thumbnails.high.url,
+        channelLink: `https://www.youtube.com/channel/${channel.channelId}`,
+      }
+      await updateChannel({ channel: toBeUpdated })
     }
 
     revalidatePath('/')
 
-    return NextResponse.json({ channels })
+    return NextResponse.json({
+      channels: channels.map(({ videoIds, ...channel }) => ({
+        ...channel,
+        videoIds: videoIds?.length ?? 0,
+      })),
+    })
   } catch (error: unknown) {
     console.error(error)
     return NextResponse.json({ error: 'Internal server error' }, { status: 500 })
