5b53783b7871c99e5dae30f9a65baf6cedf44dd5
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-10-24 16:40:54 +0200

    load-channels endpoint

---
+++ src/app/api/load-channels/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest } from 'next/server'
+import { z } from 'zod'
+import { createMongoDbClient } from '../../../clients/mongodb'
+import { Channel, channelSchema } from '../../../models/channel'
+import { isApiRequestAuthenticated } from '../../../utils/api-helpers'
+import { SERVER_ENV } from '../../../utils/server-env'
+
+const requestBodySchema = z.object({
+  channels: z.array(channelSchema as z.ZodType<Channel>).min(1),
+})
+
+export const POST = async (request: NextRequest) => {
+  if (!isApiRequestAuthenticated(request)) {
+    return { status: 401, json: { error: 'Missing or bad authorization header' } }
+  }
+
+  const requestJson = await request.json()
+  const requestBodyResult = requestBodySchema.safeParse(requestJson)
+  if (!requestBodyResult.success) {
+    return { status: 400, json: { error: 'Missing or bad request body' } }
+  }
+
+  const { channels } = requestBodyResult.data
+
+  const dbClient = createMongoDbClient({
+    connectionString: SERVER_ENV.MONGODB_URI,
+  })
+  // NOTE: This is slow an imperative, but we avoid extending the db-client interface any further.
+  for (const channel of channels) {
+    await dbClient.updateChannel({ channel })
+  }
+  return new Response(`Saved/updated ${channels.length} channels`, { status: 200 })
+}
