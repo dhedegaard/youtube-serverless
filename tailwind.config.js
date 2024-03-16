b2007b3ec8dce02db4d4be0a436f7d6b0774e715
Dennis Hedegaard <dennis@dhedegaard.dk>
2024-03-16 20:07:20 +0100

    more daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
-};
+  plugins: [require('daisyui')],
+  /** @type {import('daisyui').Config} */
+  daisyui: {
+    themes: ['light'],
+    logs: false,
+  },
+}
