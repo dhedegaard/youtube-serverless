0a02e9d612a85d19e0e54bb39441f7673dd3052b
Dennis Hedegaard <dennis@dhedegaard.dk>
2024-05-14 20:23:56 +0200

    slight refactoring

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,9 +2,9 @@ import { SVGProps, memo } from 'react'
 
 export const Logo = memo(function Logo(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg viewBox="0 0 20 20" {...props}>
-      <circle r={10} cx={10} cy={10} fill="#22BCDA" />
-      <polygon fill="#fff" points="6,4 16,10 6,16" />
+    <svg viewBox="0 0 20 20" fill="#22BCDA" color="#fff" {...props}>
+      <circle r={10} cx={10} cy={10} fill="currentFill" />
+      <polygon fill="currentColor" points="6,4 16,10 6,16" />
     </svg>
   )
 })
