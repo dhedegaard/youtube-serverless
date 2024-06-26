ba119ec10712bfaf4df6eec8c40804729f70cfcd
Dennis Hedegaard <dennis@dhedegaard.dk>
2024-06-26 16:53:57 +0200

    padding fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,12 +16,12 @@ const Index: NextPage = () => {
   return (
     <>
       <nav className="mb-4 flex w-full items-end gap-4 bg-gray-700 py-4 text-white">
-        <div className="mx-auto flex w-full max-w-7xl flex-wrap gap-3 px-2">
+        <div className="mx-auto flex w-full max-w-7xl flex-wrap gap-3 px-4">
           <Logo className="flex-none" width={24} height={24} />
           New youtube videos
         </div>
       </nav>
-      <div className="mx-auto mb-8 grid max-w-7xl grid-cols-2 gap-x-6 gap-y-8 px-2 md:grid-cols-3 lg:grid-cols-4">
+      <div className="mx-auto mb-8 grid max-w-7xl grid-cols-2 gap-x-4 gap-y-8 px-4 md:grid-cols-3 lg:grid-cols-4">
         {videos.map((video) => (
           <VideoElement key={video.videoId} video={video} />
         ))}
