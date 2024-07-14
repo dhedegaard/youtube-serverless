c4855bcff92644e6807cf15122d87d1093a13c21
Dennis Hedegaard <dennis@dhedegaard.dk>
2024-07-14 22:53:08 +0200

    refresh hourly

--- src/app/fetchers.ts
+++ src/app/fetchers.ts
@@ -1,17 +1,21 @@
 'use client'
 
-import useSWR, { SWRResponse } from 'swr'
-import { useEffect, useMemo } from 'react'
+import { useEffect } from 'react'
+import useSWR, { SWRConfiguration, SWRResponse } from 'swr'
 import { getVideos, GetVideosResult } from './actions'
+
+const swrConfig: SWRConfiguration = {
+  // 1 hour
+  refreshInterval: 3_600_000,
+}
 
 interface VideoKey {
   key: 'videos'
 }
+const key: VideoKey = { key: 'videos' }
+
 export const useVideos = (): SWRResponse<GetVideosResult, unknown> => {
-  const response = useSWR<GetVideosResult, unknown, VideoKey>(
-    useMemo<VideoKey>(() => ({ key: 'videos' }), []),
-    getVideos
-  )
+  const response = useSWR<GetVideosResult, unknown, VideoKey>(key, getVideos, swrConfig)
 
   useEffect(() => {
     if (response.error != null) {
