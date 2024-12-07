abdc284bea02fe8707d0c3eaaf59ff4c080cb221
Dennis Hedegaard <dennis@dhedegaard.dk>
2024-12-07 14:10:32 +0100

    removed SWR, bumps all around

--- src/app/VideoElements.tsx
+++ src/app/VideoElements.tsx
@@ -1,21 +1,12 @@
 'use client'
 
-import { memo, useEffect, useMemo } from 'react'
+import { memo } from 'react'
 import { Video } from '../models/video'
-import { useVideos } from './fetchers'
 import { VideoElement } from './VideoElement'
 
 interface VideoElementsProps {
-  initialVideos: readonly Video[]
+  videos: readonly Video[]
 }
-export const VideoElements = memo<VideoElementsProps>(function VideoElements({ initialVideos }) {
-  const { data } = useVideos()
-
-  useEffect(() => {
-    console.log('DATA:', data)
-  }, [data])
-
-  const videos = useMemo(() => data?.videos ?? initialVideos, [data?.videos, initialVideos])
-
+export const VideoElements = memo<VideoElementsProps>(function VideoElements({ videos }) {
   return videos.map((video) => <VideoElement key={video.videoId} video={video} />)
 })
